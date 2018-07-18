f303800576ea9ac8baf155aa4bfeb3581377bf34
Darren Young <dlyoung5@asu.edu>
2018-07-18 03:18:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = new Object()
+
+function updateObjectWithKeyAndValue(obj, key, value){
+  return Object.assign({}, obj, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  var obj = (obj.key = value)
+  return obj
+}
+
+
+function deleteFromObjectByKey(obj, key){
+  var obj = new Object()
+  var newObj = Object.assign({}, obj)
+  return obj
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key){
+  var obj = new Object()
+  var newObj = delete obj.key
+  delete obj.key
+  return obj
+}
